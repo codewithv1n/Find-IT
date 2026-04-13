3c2a3b828ded31a0c94d4178866ee9397f8c63c5
codewithv1n <vinzelmarano@gmail.com>
2026-04-13 18:38:04 +0800

    UPDATE: refactor and add another logic here using stmt but now request method

--- controllers/user_main_process.php
+++ controllers/user_main_process.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include '../controllers/connect_db.php';
 
 // Ito yung logic ko for username, joined date displaying :>
@@ -27,6 +28,29 @@ if (isset($_SESSION['user_id'])) {
     $joinedDate = "N/A";
 }
 
+// ito naman yung ginawa ko para sa report_item para makapag post direkta sa database
+if ($_SERVER["REQUEST_METHOD"] === "POST"){
+
+ $author = $_POST['username'];
+ $itemName = $_POST['item_name'];
+ $itemDescription = $_POST['item_description'];
+ $itemCategory = $_POST['item_category'];
+ $itemDate = $_POST['item_date'];
+
+
+ $stmt = $conn->prepare("INSERT INTO reported_items (username, item_name, item_description, item_category, item_date) VALUES (?, ?, ?, ?, ?)");
+ $stmt->bind_param("sssss", $author, $itemName, $itemDescription, $itemCategory, $itemDate);
+ $stmt->execute();
+ 
+ if($stmt->affected_rows > 0){
+    echo "Item reported successfully";
+    header("Location: ../users/dashboard.php");
+ }else{
+    echo "Failed to report item";
+ }
+ 
+ exit();
+}
 ?>
 
 
