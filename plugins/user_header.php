7fe168771cec9a9f943925f2cc08fecf94552702
codewithv1n <vinzelmarano@gmail.com>
2026-04-18 23:57:02 +0800

    UPDATE:  i refactor the code into a plug and play version. I erase unnecessary code and make it clean.

--- plugins/user_header.php
+++ plugins/user_header.php
@@ -1,26 +1,14 @@
 <?php
-session_start();
 include '../controllers/connect_db.php';
 include '../controllers/user_main_process.php';
-
-
-if (!isset($_SESSION['user_id'])) {
-    header("Location: login.php");
-    exit();
-}
-
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="../plugins/css/user_header_design.css">
-</head>
-<body>
+
 <header>
+  <div class="toggle-btn" id="toggleSidebar">
+    <i class="fa fa-bars"></i>
+  </div>
+
   <h1>FIND IT</h1>
 
   <div class="notification-bell">
@@ -28,9 +16,8 @@ if (!isset($_SESSION['user_id'])) {
   </div>
 
   <div class="user-info">
-     <i class="fa fa-user-circle-o">
+     <i class="fa fa-user-circle-o"></i>
      <h5><?php echo htmlspecialchars($userName); ?></h5>
-    </i>
   </div>
 
   <div class="user-actions">
@@ -38,7 +25,5 @@ if (!isset($_SESSION['user_id'])) {
   </div>
 </header>
 
-<body>
-
 <script src="../plugins/js/user_logout_functions.js"></script>
-</html>
+<script src="../plugins/js/user_sidebar_functions.js"></script>
--- plugins/user_sidebar.php
+++ plugins/user_sidebar.php
@@ -1,19 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/user_sidebar_design.css">
-</head>
-
-<body>
- 
-<div class="sidebar">
-      
+<script>
+(function() {
+    if (window.innerWidth > 1024 && localStorage.getItem('sidebarState') === 'collapsed') {
+        document.body.classList.add('sidebar-collapsed');
+    }
+})();
+</script>
+<div class="sidebar" id="sidebar">     
    <div class="logo-container">
     <img src="../images/logo/Find IT.png" alt="logo">
    </div>
-
 
     <ul>
         <li><a href="dashboard.php">Dashboard</a></li>
@@ -22,8 +17,5 @@
         <li><a href="my_posts.php">My Posts</a></li>
         <li><a href="about.php">About</a></li>
     </ul>
+
 </div>
-
-
-</body>
-</html>
