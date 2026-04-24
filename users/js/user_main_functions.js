ddffb5235adf0817047b711b319ff6ec2e81da6f
codewithv1n <vinzelmarano@gmail.com>
2026-04-24 21:49:35 +0800

    UPDATE: refactoring the report item dom event listener putting alerts timeout and make a modal for edit post

--- users/js/user_main_functions.js
+++ users/js/user_main_functions.js
@@ -1,9 +1,35 @@
-// Report Item button function
+// Report Item button DOM manipulation
 document.addEventListener("DOMContentLoaded", () => {
-    const btnReport = document.getElementById("btn-report");
-    if(btnReport){
+    const btnReport = document.getElementById("btnReport");
+    if (btnReport) {
         btnReport.addEventListener("click", () => {
-            btnReport.innerHTML = "Reporting..."
-        })
+            btnReport.innerHTML = "Reporting...";
+        });
+    }
+
+    // hide alerts 
+    const alerts = document.querySelectorAll(".alert-box, .alert-box-report-item, .alert-box-my-post");
+    alerts.forEach(alert => {
+        setTimeout(() => {
+            alert.style.display = "none";
+        }, 3000); 
+    });
+});
+
+
+// modal for edit ng post
+document.addEventListener("click", function(event) {
+    if (event.target.classList.contains("btn-edit")) {
+        var targetModalId = event.target.getAttribute("data-modal");
+        var modal = document.getElementById(targetModalId);
+        modal.style.display = "block";
+    }
+
+    if (event.target.classList.contains("close")) {
+        event.target.closest(".modal").style.display = "none";
+    }
+    
+    if (event.target.classList.contains("modal")) {
+        event.target.style.display = "none";
     }
 });
